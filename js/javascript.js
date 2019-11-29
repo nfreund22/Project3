9187095f97ba35a8631b07765dd63c3ca0ba77a0
nfreund22 <32681397+nfreund22@users.noreply.github.com>
2019-11-29 14:46:31 -0600

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,7 @@ var mapProp= {
   center:new google.maps.LatLng(51.508742,-0.120850),
   zoom:5,
 };
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var map = new google.maps.Map(document.getElementById("map"),mapProp);
 }
 
 function runScript() {
