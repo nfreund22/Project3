16ef64f9ce134aa2d41c58e340dfebc7e593b94a
nfreund22 <32681397+nfreund22@users.noreply.github.com>
2019-11-29 17:01:37 -0600

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,53 +1,7 @@
-// Initialize and add the map
-
-function initMap() {
-
-  // The location of Everest
-
-  var everest = {lat: 27.988271, lng: 86.924932};
-
-  // The location of Everest view hotel
-
-  var view = {lat: 27.816692, lng: 86.723680};
-
-  // The location of Little Flowers' School
-
-  var school = {lat: 26.631896, lng: 87.990280};
-
-  var options = {
-
-      zoom: 8,
-
-      center: everest,
-
-      mapTypeId: google.maps.MapTypeId.TERRAIN   
-
-    };
-
-  // The map, centered at Everest
-
-  var map = new google.maps.Map(
-
-      document.getElementById('map'), options);
-
-  // The marker, positioned at Everest
-
-  var marker = new google.maps.Marker({position: everest, map: map});
-
-  // The marker, positioned at Everest view hotel
-
-  var marker = new google.maps.Marker({position: view, map: map});
-
-  // The marker, positioned at Little Flowers' School
-
-  var marker = new google.maps.Marker({position: school, map: map});
-
-  
-
-  
-
-}
-
-
-
-google.maps.event.addDomListener(window, 'load', initMap);
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
