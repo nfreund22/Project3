85d0a6245fac63a093ec401b6d34d793cc511ea6
nfreund22 <32681397+nfreund22@users.noreply.github.com>
2019-12-01 15:00:23 -0600

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,12 @@
 var map;
       function initMap() {
+            var city = {lat: 42.110, lng: -88.034};
             map = new google.maps.Map(document.getElementById('map'), {
-                  center: {lat: 42.110, lng: -88.034},
+                  center: city,
                   zoom: 8
             });
             var marker1 = new google.maps.Marker({
-                  position: {lat: 42.110, lng: -88.034};
+                  position: city,
                   map: map,
                   title: "My Hometown"
             });
