282cd4810dd6b814cd0187a0b17c16483839b5a2
nfreund22 <32681397+nfreund22@users.noreply.github.com>
2019-12-01 15:17:57 -0600

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,28 @@
 var map;
       function initMap() {
+            var marker1Info = '<p><b>Palatine</b>, is my hometown.' +
+                'Palatine is a village in Cook County, Illinois, United States.' +
+                'It is a northwestern residential suburb of Chicago.' +
+                'As of the 2000 census, the village had a total population of 65,479.</p>';
+            
+            var infoWindow1 = new google.maps.InfoWindow({
+                  content: marker1Info
+            });
+            
             var city = {lat: 42.110, lng: -88.034};
+            
             map = new google.maps.Map(document.getElementById('map'), {
                   center: city,
-                  zoom: 8
+                  zoom: 12
             });
+            
             var marker1 = new google.maps.Marker({
                   position: city,
                   map: map,
                   title: "My Hometown"
+            });
+            
+            marker1.addListener('click', function() {
+                  infoWindow1.open(map,marker);
             });
       }
