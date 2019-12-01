7283b52908019358fb49bfba401c62aeac0c76ff
nfreund22 <32681397+nfreund22@users.noreply.github.com>
2019-12-01 15:46:49 -0600

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -25,4 +25,15 @@ var map;
             marker1.addListener('click', function() {
                   infoWindow1.open(map, marker1);
             });
+            
+            var cityCircle = new google.maps.Circle({
+                  strokeColor: '#FF0000',
+                  strokeOpacity: 0.8,
+                  strokeWeight: 2,
+                  fillColor: 'FF0000',
+                  fillOpacity: 0.35,
+                  map: map,
+                  center: city,
+                  radius: 100
+            });
       }
