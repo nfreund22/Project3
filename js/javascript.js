7b5640430a82281c8237ad20e67bdcd3b2c1f0be
nfreund22 <32681397+nfreund22@users.noreply.github.com>
2019-12-01 16:41:41 -0600

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -30,7 +30,7 @@ var map;
                   strokeColor: '#FF0000',
                   strokeOpacity: 0.8,
                   strokeWeight: 2,
-                  fillColor: 'FF0000',
+                  fillColor: '#FF0000',
                   fillOpacity: 0.35,
                   map: map,
                   center: city,
