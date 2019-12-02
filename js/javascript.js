ab0c71a931f8e6709284d6e8584b8f8c12ce467f
nfreund22 <32681397+nfreund22@users.noreply.github.com>
2019-12-02 15:00:30 -0600

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -13,7 +13,7 @@ var map;
             
             map = new google.maps.Map(document.getElementById('map'), {
                   center: city,
-                  zoom: 10
+                  zoom: 8
             });
             
             var marker1 = new google.maps.Marker({
